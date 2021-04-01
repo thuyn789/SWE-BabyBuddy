fb3b150dacdc4dd71dc6ef2131211b4523ecd33a
Samantha Newkirk <60722173+samnewkirk@users.noreply.github.com>
2021-04-01 19:11:06 -0400

    Login/Logout Progress

--- app/src/main/java/com/example/messengerapp/MainPageActivity.java
+++ app/src/main/java/com/example/messengerapp/MainPageActivity.java
@@ -1,17 +1,27 @@
 package com.example.messengerapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.Manifest;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.budiyev.android.codescanner.CodeScanner;
+
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.budiyev.android.codescanner.ErrorCallback;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.zxing.Result;
 
 public class MainPageActivity extends AppCompatActivity {
+
+    private CodeScanner mCodeScanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +41,58 @@ public class MainPageActivity extends AppCompatActivity {
             }
         });
 
+        codeScanner();
         getPermissions();
 
     }//end create
+
+    private void codeScanner() {
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainPageActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        //when camera fail to initialize/scan
+        mCodeScanner.setErrorCallback(new ErrorCallback() {
+            @Override
+            public void onError(@NonNull Exception error) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainPageActivity.this, "Camera Failed!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        //Camera begin scan
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCodeScanner.startPreview();
+            }
+        });
+    }//codeScanner
+
+    protected void onResume(){
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    protected void onPause(){
+        mCodeScanner.releaseResources();
+        super.onPause();
+    }
 
     private void getPermissions() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
