0221ffaa484c5b1a242b1c2eeb86aeaf2ae17a3c
thuyn789 <thuynh0135@gmail.com>
2021-04-07 12:11:51 -0400

    Added methods to retrieve data from User Object

--- app/src/main/java/com/example/babybuddy/User.java
+++ app/src/main/java/com/example/babybuddy/User.java
@@ -5,15 +5,33 @@ package com.example.babybuddy;
 
 public class User {
     //Initialize public variables
-    public String first_name, last_name, phone_number, email;
+    public String first_name, last_name, phone_number, email, qrcode;
 
     //Initialize empty constructor
     public User(){}
 
+    //Initialize constructor
     public User(String first_name, String last_name, String phone_number, String email){
         this.first_name = first_name;
         this.last_name = last_name;
         this.phone_number = phone_number;
         this.email = email;
+    }
+
+    public String getPhone() {
+        return phone_number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getQrcode() {
+        return qrcode;
+    }
+    
+    //The QR Code is not meant to be initialized by the constructor but by this method
+    public void setQrcode(String qrcode){
+        this.qrcode = qrcode;
     }
 }
